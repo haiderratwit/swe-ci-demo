8c578a5c67a2a692267ae74207e12eaf9fbdd96a
Rida Haider <57573664+haiderratwit@users.noreply.github.com>
2022-03-30 23:21:06 -0400

    FizzBuzz Test 1.2

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -5,29 +5,18 @@ public class FizzBuzz
     String processNumber(int num)
 	{	
     	// Copy your code here
-	    if (num == 3){
-		    return "Fizz";
-	    }
-	      if (num == 6){
-		    return "Fizz";
-	    }
-	      if (num == 5){
-		    return "Buzz";
-	    }
-	      if (num == 10){
-		    return "Buzz";
-	    }
-	      if (num == 15){
-		    return "FizzBuzz";
-	    }
-	      if (num == 30){
-		    return "FizzBuzz";
-	    }
-	  
-	    return String.valueOf(num);
 	    
-	}
-	
+	   if (number % 3 == 0 && number % 5 == 0) {
+		   return "FizzBuzz";
+	   }
+	    if (number % 3 == 0) {
+		    return "Fizz";
+	    }
+	    if (number % 5 == 0) {
+		    return "Buzz";
+	    }
+	    return String.valueOf(number);
+    }
 	String processNumbers (int[] numList)
 	{
     	// Copy your code here
