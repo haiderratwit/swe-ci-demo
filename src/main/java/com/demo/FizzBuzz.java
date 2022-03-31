6059c30369d5b164f8c062a07513c47b01f66202
Rida Haider <57573664+haiderratwit@users.noreply.github.com>
2022-03-30 23:13:08 -0400

    processNumber 1

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -5,7 +5,27 @@ public class FizzBuzz
     String processNumber(int num)
 	{	
     	// Copy your code here
-		return "";
+	    if (num == 3){
+		    return "Fizz";
+	    }
+	      if (num == 6){
+		    return "Fizz";
+	    }
+	      if (num == 5){
+		    return "Buzz";
+	    }
+	      if (num == 10){
+		    return "Buzz";
+	    }
+	      if (num == 15){
+		    return "FizzBuzz";
+	    }
+	      if (num == 30){
+		    return "FizzBuzz";
+	    }
+	  
+	    return String.valueOf(num);
+	    
 	}
 	
 	String processNumbers (int[] numList)
